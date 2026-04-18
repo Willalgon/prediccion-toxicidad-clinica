495c4ca6bb9b298339a04df16a6d21c215b73320
Guillermo Álvarez González <guillermoalvarezgonzalez06@gmail.com>
2026-04-18 19:02:11 +0200

    DataLoader

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,20 +4,20 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np # utilizaremos float32 bits para torch
 import torch
-from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
-from rdkit import Chem
-from rdkit import rdMolDescriptors
+from torch.utils.data import Dataset, DataLoader # Dataset (interfaz para heredar len y getitem) # DataLoader para cargar los batches
+from sklearn.model_selection import train_test_split 
+from rdkit import Chem # Chem entiende la química de los SMILES
+from rdkit import rdMolDescriptors # Para generar los vectores Morgan Fingerprint
 
 
-class ClinToxDataset(Dataset):
-    def __init__(self, dataframe, max_bits=2048):
+class ClinToxDataset(Dataset): # obtenemos los datos, longitud, y obtenemos las muestras ya vectorizadas
+    def __init__(self, dataframe):
         self.data = dataframe
         self.smiles = self.data["smiles"].values
         self.labels = self.data["CT_TOX"].values
-        self.max_bits = max_bits
+
 
     def __len__(self):
         return len(self.labels)
@@ -25,14 +25,12 @@ class ClinToxDataset(Dataset):
     def smiles_to_fingerprint(self, smiles_str):
         mol = Chem.MolFromSmiles(smiles_str)
         if mol is None:
-            return np.zeros(self.max_bits, dtype=np.float32)
+            return np.zeros(2048, dtype=np.float32)
         # Morgan Fingerprint
-        fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(
-            mol, radius=2, nBits=self.max_bits
-        )
-        return np.array(fp, dtype=np.float32)
+        fingerprint = rdMolDescriptors.GetMorganFingerprintAsBitVect( mol, radius=2, nBits=2048)
+        return np.array(fingerprint, dtype=np.float32)
 
-    def __getitem__(self, id):
+    def __getitem__(self, id): # 
         smile_str = self.smiles[id]
         label = self.labels[id]
         # Transformamos la química en un vector
@@ -45,15 +43,12 @@ class ClinToxDataset(Dataset):
 
 def get_dataloaders(csv_path, batch_size=32):
     df = pd.read_csv(csv_path)
-    df = df.dropna(subset=["smiles", "CT_TOX"])
+    df = df.dropna() # para eliminar de las columnas sibset las filas sin contenido
     # Dividimos
-    train_val_df, test_df = train_test_split(
-        df, test_size=0.15, random_state=42, stratify=df["CT_TOX"]
-    )
+    train_val_df, test_df = train_test_split(df, test_size=0.15, random_state=42, stratify=df["CT_TOX"])
+    # Queremos el mismo random state siempre para guardar la distribución y no cambiar los resultados de las métricas
     # Obtenemos conjunto de validación
-    train_df, val_df = train_test_split(
-        train_val_df, test_size=0.15, random_state=42, stratify=train_val_df["CT_TOX"]
-    )
+    train_df, val_df = train_test_split( train_val_df, test_size=0.15, random_state=42, stratify=train_val_df["CT_TOX"])
 
     train_ds = ClinToxDataset(train_df)
     val_ds = ClinToxDataset(val_df)
@@ -66,5 +61,4 @@ def get_dataloaders(csv_path, batch_size=32):
     print(
         f"Dataset is ready: {len(train_df)} Train, {len(val_df)} Validation, {len(test_df)} Test"
     )
-
     return train_loader, val_loader, test_loader
